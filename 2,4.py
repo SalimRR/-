036dff8ecc2bd48d89e94b5b2b45e4490b474018
SalimRR <72098955+SalimRR@users.noreply.github.com>
2020-10-28 21:16:50 +0300

    Add files via upload

---
+++ 2,4.py
@@ -0,0 +1,2 @@
+def func(a=5, b=3, c=2):
+    return (a+b+c)/3
---
+++ 8_2.py
@@ -0,0 +1,9 @@
+s = str(input(''))
+n = 10
+def stroka(s, n):
+    d = len(s)
+    if d > n:
+        print(s.upper())
+    else:
+        print(s)
+stroka(s, n)
